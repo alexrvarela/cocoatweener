1d1d78b368665000c1e02955367c2088df26d8d3
Alejandro R. Varela <alejandro.varela@sugarcotton.com>
2018-09-06 12:19:24 -0500

    samples updated

--- Example/Examples/ViewController.m
+++ Example/Examples/ViewController.m
@@ -25,6 +25,8 @@
 #import "ArcOrbits.h"
 #import "SimpleTween.h"
 #import "TweenHandlers.h"
+#import "DragView.h"
+#import "SimpleTimeline.h"
 
 @interface ViewController ()
 
@@ -44,7 +46,6 @@
         self.container = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
         [self.view addSubview:self.container];
         
-//        float spacing = 20.0f;
         float buttonWidth = (UIScreen.mainScreen.bounds.size.width) / 2.0f - 1.0;
         
         UIButton* prevButton = [[UIButton alloc] initWithFrame:CGRectMake(0.0f,
@@ -64,27 +65,27 @@
         [nextButton addTarget:self action:@selector(next) forControlEvents:UIControlEventTouchUpInside];
         nextButton.backgroundColor = UIColor.blackColor;
         [self.view addSubview:nextButton];
-
         
-//        CGRect contentFrame = UIScreen.mainScreen.bounds;
         CGRect contentFrame = CGRectMake(0.0f,
-                                        0.0f,
-                                        UIScreen.mainScreen.bounds.size.width,
-                                        UIScreen.mainScreen.bounds.size.height - 40.0f);
+                                         0.0f,
+                                         UIScreen.mainScreen.bounds.size.width,
+                                         UIScreen.mainScreen.bounds.size.height - 40.0f);
         
         [self.container addSubview:[[SimpleTween alloc] initWithFrame:contentFrame]];
         [self.container addSubview:[[TweenHandlers alloc] initWithFrame:contentFrame]];
-        [self.container addSubview:[[ArcOrbits alloc] initWithFrame:contentFrame]];
-        [self.container addSubview:[[AnimateArcRadius alloc] initWithFrame:contentFrame]];
-        [self.container addSubview:[[ScrollAims alloc] initWithFrame:contentFrame]];
-        [self.container addSubview:[[PathLoop alloc] initWithFrame:contentFrame]];
         [self.container addSubview:[[TouchPoint alloc] initWithFrame:contentFrame]];
-        [self.container addSubview:[[AnimateText alloc] initWithFrame:contentFrame]];
+        [self.container addSubview:[[DragView alloc] initWithFrame:contentFrame]];
         [self.container addSubview:[[PauseTweens alloc] initWithFrame:contentFrame]];
-        [self.container addSubview:[[WindBlow alloc] initWithFrame:contentFrame]];
+        [self.container addSubview:[[SimpleTimeline alloc] initWithFrame:contentFrame]];
         [self.container addSubview:[[ScrollTimeline alloc] initWithFrame:contentFrame]];
         [self.container addSubview:[[TimelineBasic alloc] initWithFrame:contentFrame]];
-
+        [self.container addSubview:[[AnimateArcRadius alloc] initWithFrame:contentFrame]];
+        [self.container addSubview:[[PathLoop alloc] initWithFrame:contentFrame]];
+        [self.container addSubview:[[WindBlow alloc] initWithFrame:contentFrame]];
+        [self.container addSubview:[[ArcOrbits alloc] initWithFrame:contentFrame]];
+        [self.container addSubview:[[AnimateText alloc] initWithFrame:contentFrame]];
+        [self.container addSubview:[[ScrollAims alloc] initWithFrame:contentFrame]];
+        
         float x = 0.0f;
         
         for (UIView* view in self.container.subviews)
@@ -107,24 +108,19 @@
     nFrame.origin.x = - (pageIndex * w);
     
     if([[self.container.subviews objectAtIndex:pageIndex] respondsToSelector:@selector(freeze)])
-       [[self.container.subviews objectAtIndex:pageIndex] performSelector:@selector(freeze)];
-        
+        [[self.container.subviews objectAtIndex:pageIndex] performSelector:@selector(freeze)];
+    
     _pageIndex = pageIndex;
     
     [CocoaTweener removeTweens:self.view];//TODO: remove for key
     [CocoaTweener addTween:[[Tween alloc] init:self.container
-                                       duration:0.5f
-                                           ease:kEaseOutCubic
-                                           keys:@{
-                                                  @"frame":[NSValue valueWithCGRect:nFrame],//Add key with destination value
-                                                  }
-                                          delay:0.0f
-                             ]];
-
-}
-
--(void)freeze
-{
+                                      duration:0.5f
+                                          ease:kEaseOutCubic
+                                          keys:@{
+                                                 @"frame":[NSValue valueWithCGRect:nFrame],//Add key with destination value
+                                                 }
+                                         delay:0.0f
+                            ]];
     
 }
 
@@ -148,7 +144,6 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    //PAUSE ALL TWEENS
 }
 
 -(BOOL)prefersStatusBarHidden

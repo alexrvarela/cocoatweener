2f83f6ac00e181d842ab09c5adf829fe53462b2c
Alejandro R. Varela <alejandro.varela@sugarcotton.com>
2018-09-05 21:39:19 -0500

    readme file updated

---
+++ Example/Samples/TimelineExample.h
@@ -0,0 +1,13 @@
+//
+//  TimelineExample.h
+//  Examples
+//
+//  Created by Alejandro Ramirez Varela on 9/5/18.
+//  Copyright © 2018 Alejandro Ramirez Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineExample : UIView
+
+@end
---
+++ Example/Samples/TimelineExample.m
@@ -0,0 +1,21 @@
+//
+//  TimelineExample.m
+//  Examples
+//
+//  Created by Alejandro Ramirez Varela on 9/5/18.
+//  Copyright © 2018 Alejandro Ramirez Varela. All rights reserved.
+//
+
+#import "TimelineExample.h"
+
+@implementation TimelineExample
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
